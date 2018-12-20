cba84ec4fb830bd1d37ee91f5b8161c268e1e3e5
Dipesh Acharya <xtranophilist@gmail.com>
2018-12-21 03:14:21 +0545

    Integrate vuex

--- store.js
+++ store.js
@@ -1,10 +1,22 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import VuexPersist from 'vuex-persist';
+
 // import { getBase64 } from './utils'
 
 Vue.use(Vuex);
 
+const vuexLocalStorage = new VuexPersist({
+  key: 'vuex', // The key to store the state on in the storage provider.
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  // Function that passes the state and returns the state with only the objects you want to store.
+  // reducer: state => state,
+  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
+  // filter: mutation => (true)
+})
+
 const store = new Vuex.Store({
+  plugins: [vuexLocalStorage.plugin],
   state: {
     selected: null,
     images: []
