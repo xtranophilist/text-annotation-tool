4bf89b90db9c2e7f29a460c5b6433463a1f87d6f
Dipesh Acharya <xtranophilist@gmail.com>
2018-12-23 16:49:35 +0545

    Use IndexedDB with localForage

--- store.js
+++ store.js
@@ -3,16 +3,16 @@ import Vue from 'vue'
 import VuexPersist from 'vuex-persist'
 
 // import { getBase64 } from './utils'
+import localforage from 'localforage'
+
 
 Vue.use(Vuex);
 
 const vuexLocalStorage = new VuexPersist({
   key: 'vuex', // The key to store the state on in the storage provider.
-  storage: window.localStorage, // or window.sessionStorage or localForage
-  // Function that passes the state and returns the state with only the objects you want to store.
-  // reducer: state => state,
-  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
-  // filter: mutation => (true)
+  storage: localforage,
+  saveState: (key, state, storage) => Promise.resolve(storage.setItem(key, state)),
+  restoreState: (key, storage) => Promise.resolve(storage.getItem(key))
 })
 
 const store = new Vuex.Store({
